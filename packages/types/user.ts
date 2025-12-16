70e1704bc39e398b07d244619d2f6c5518f3eeb4
eeekeee <ekrtionl@gmail.com>
2025-12-16 14:54:38 +0900

    feat: add socket event, user and room type definitions (#4)
    Co-authored-by: DaYeonKim <dyeon-dev@users.noreply.github.com>
    Co-authored-by: Lee Junsub <lee980605@knu.ac.kr>
    Co-authored-by: 김채영 <cchaeyoung@users.noreply.github.com>

---
+++ packages/types/user.ts
@@ -0,0 +1,14 @@
+export type UserRole = 'player' | 'spectator';
+
+// 방에 들어가지 않은 사용자
+export interface User {
+    userId: string;
+    username: string;
+}
+
+// 방에 들어간 사용자
+export interface RoomUser extends User {
+    socketId: string;
+    role: UserRole;
+    joinedAt: Date;
+}
