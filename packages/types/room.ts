199153558085945edaa4eb10845d3144ee06108c
cchaeyoung <kimchaeyoung05@naver.com>
2025-12-17 02:34:20 +0900

    chore: Prettier 줄바꿈 설정을 'auto'로 변경 (#1)

--- packages/types/room.ts
+++ packages/types/room.ts
@@ -1,28 +1,28 @@
-import { RoomUser } from "./user";
+import { RoomUser } from './user';
 
 export type RoomStatus = 'waiting' | 'in-battle';
 
 export interface Room {
-    roomId: string;
-    title: string;
-    hostId: string;
-    status: RoomStatus;
-    createdAt: Date;
-    settings: RoomSettings;
-    // 현재 접속한 유저 리스트
-    currentPlayers: RoomUser[];
-    currentSpectators: RoomUser[];
+  roomId: string;
+  title: string;
+  hostId: string;
+  status: RoomStatus;
+  createdAt: Date;
+  settings: RoomSettings;
+  // 현재 접속한 유저 리스트
+  currentPlayers: RoomUser[];
+  currentSpectators: RoomUser[];
 }
 
 // 방 설정(방 생성)
 export interface RoomSettings {
-    maxPlayers: number;
-    password?: string;
-    timeout?: number;
+  maxPlayers: number;
+  password?: string;
+  timeout?: number;
 }
 
 export interface RoomCreateDTO {
-    title: string;
-    hostId: string;
-    settings: RoomSettings;
+  title: string;
+  hostId: string;
+  settings: RoomSettings;
 }
