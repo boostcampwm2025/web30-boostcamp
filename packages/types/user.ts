3902906b43bf837e737873fc816aaf6d972dda1f
Junsub Lee <lee980605@knu.ac.kr>
2025-12-18 00:29:46 +0900

    chore: lucide 아이콘 추가 (#13) - package.json, pnpm-lock.yaml 의존성 추가 - lint, prettier 충돌 수정

--- packages/types/user.ts
+++ packages/types/user.ts
@@ -2,13 +2,13 @@ export type UserRole = 'player' | 'spectator';
 
 // 방에 들어가지 않은 사용자
 export interface User {
-    userId: string;
-    username: string;
+  userId: string;
+  username: string;
 }
 
 // 방에 들어간 사용자
 export interface RoomUser extends User {
-    socketId: string;
-    role: UserRole;
-    joinedAt: Date;
+  socketId: string;
+  role: UserRole;
+  joinedAt: Date;
 }
